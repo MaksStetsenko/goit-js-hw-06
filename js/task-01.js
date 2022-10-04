122a085fd26b5ff26a8cfd0d1ea31d0a343d006c
MaksStetsenko <106399273+MaksStetsenko@users.noreply.github.com>
2022-10-04 18:06:08 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -6,8 +6,6 @@ console.log(`Number of categories: ${categories.length}`);
 
 console.log(" ");
 
-//============================================
-
 categories.forEach((category) => {
   const items = [...category.children];
   items.forEach((item) => {
@@ -22,4 +20,17 @@ categories.forEach((category) => {
     }
   });
   console.log(" ");
+});
+
+//=================Alternative====================================
+
+const list = document.querySelector("#categories");
+const items = list.querySelectorAll(".item");
+console.log(`В списке ${items.length} категории:`);
+items.forEach((item) => {
+  const h2 = item.querySelector("h2");
+  const items = item.querySelectorAll("li");
+  console.log(
+    ` - категория: ${h2.textContent} (количество элементов: ${items.length})`
+  );
 });
